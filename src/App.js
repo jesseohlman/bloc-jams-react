a74f362232115e136bd572cb95825745d18fbb82
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2018-12-21 09:31:32 -0500

    fixed errors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import {Route, Link} from 'react-router-dom';
 import Library from './components/Library';
 import Landing from './components/Landing';
+import Album from './components/Album';
 import './App.css';
 
 class App extends Component {
@@ -12,12 +13,12 @@ class App extends Component {
       <header>
         <nav>
         <Link to='/'>Landing</Link>
-        <Link to='/Library'>Library</Link>
+        <Link to='/library'>Library</Link>
         </nav>
 <h1>Bloc James</h1>
 <main>
 <Route exact path="/" component={Landing} />
-<Route path="/library" component={library} />
+<Route path="/library" component={Library} />
 <Route path="/album" component={Album} />
 </main>
       </header>
---
+++ src/components/Album.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Album = () => (
+    <div>
+        <p>Album cover goes here</p>
+    </div>
+)
+
+export default Album;
